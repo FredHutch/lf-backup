4762c512689d4d2ad5867e90e803bd40f7a41c0e
Jeff Katcher <jmkatcher@yahoo.com>
2016-12-26 16:26:37 -0800

    download_from_swift

--- lf_backup/lflib.py
+++ lf_backup/lflib.py
@@ -170,6 +170,9 @@ def upload_to_swift(filename,destname,container,meta=""):
         "--segment-container=.segments_"+container,
         "--header=X-Object-Meta-Uploaded-by:"+getpass.getuser(),*final)
 
+def download_from_swift(filename,destname,container)
+    sw_download("--output="+destname,container,filename)
+
 # imported code to create connections to allow get_container calls
 
 def create_sw_conn():
